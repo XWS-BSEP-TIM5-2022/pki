04ef2e0b4db04a3465ed2e9c8bf5996c88cb312e
TeodoraMaruna <maruna.ra117.2018@uns.ac.rs>
2022-04-12 23:35:32 +0200

    feat: refactor code

--- pki/src/main/java/com/security/pki/model/CertificateUsage.java
+++
@@ -1,5 +0,0 @@
-package com.security.pki.model;
-
-//public enum CertificateUsage {
-//    keyCertSign, digitalSignature, keyEncipherment, dataEncipherment, cRLSign, keyAgreement, encipherOnly, decipherOnly
-//}
