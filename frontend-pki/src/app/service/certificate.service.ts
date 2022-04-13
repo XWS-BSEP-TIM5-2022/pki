18fb69b77e252d1b9b5269104c3ca7204aa5163a
TeodoraMaruna <maruna.ra117.2018@uns.ac.rs>
2022-04-13 11:35:35 +0200

    feat: additional constraints for user

--- frontend-pki/src/app/service/certificate.service.ts
+++ frontend-pki/src/app/service/certificate.service.ts
@@ -50,4 +50,8 @@ export class CertificateService {
   findAllRootAndCAByUser(id: number) {
     return this._http.get<Certificate[]>(`${this.userPath}/findAllRootAndCAByUser/` + id)  
   }
+
+  findAllByUserId(id: number) {
+    return this._http.get<Certificate[]>(`${this.userPath}/getAllByUser/` + id)  
+  }
 }
